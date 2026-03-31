4a783297d1aec9007a769e164dd55351f1998f3a
andreiz505 <andrzamfirescu20@gmail.com>
2026-03-31 16:02:23 +0300

    Tema casa laborator 4

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,72 @@
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        String fisierIntrare = "studenti_in.txt";
-        String fisierIesire = "studenti_out.txt";
-        List<Student> listaStudenti = new ArrayList<>();
-
+        String fisierStudenti = "studenti_in.txt";
+        String fisierNote = "note.txt";
+        Map<String, Student> mapStudenti = new HashMap<>();
         try {
-            Path pathIntrare = Paths.get(fisierIntrare);
-            List<String> linii = Files.readAllLines(pathIntrare);
-
-            for (String linie : linii) {
+            List<String> liniiStudenti = Files.readAllLines(Paths.get(fisierStudenti));
+            for (String linie : liniiStudenti) {
                 String[] bucati = linie.split(",");
-
                 if (bucati.length == 4) {
                     String matricol = bucati[0].trim();
                     String prenume = bucati[1].trim();
                     String nume = bucati[2].trim();
                     String formatie = bucati[3].trim();
-
-                    listaStudenti.add(new Student(matricol, nume, prenume, formatie));
+                    mapStudenti.put(matricol, new Student(matricol, nume, prenume, formatie));
                 }
             }
-            System.out.println("Am citit " + listaStudenti.size() + "studentii din fisier:");
-            for (Student s : listaStudenti) {
-                System.out.println(s);
-            }
-
         } catch (IOException e) {
-            System.out.println("Eroare la citire!");
-            e.printStackTrace();
-            return;
+            System.out.println("Nu se poate citi");
+        }
+        try {
+            List<String> liniiNote = Files.readAllLines(Paths.get(fisierNote));
+            for (String linie : liniiNote) {
+                String[] bucati = linie.split(",");
+                if (bucati.length == 2) {
+                    String matricol = bucati[0].trim();
+                    float nota = Float.parseFloat(bucati[1].trim());
+                    Student studentGasit = mapStudenti.get(matricol);
+                    if (studentGasit != null) {
+                        studentGasit.setNota(nota);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Nu s-a putut citi note.txt");
         }
 
-
-        Collections.sort(listaStudenti, new Comparator<Student>() {
-            @Override
-            public int compare(Student s1, Student s2) {
-                int rezultatFormatie = s1.getFormatieDeStudiu().compareTo(s2.getFormatieDeStudiu());
-                if (rezultatFormatie == 0) {
-                    return s1.getNume().compareTo(s2.getNume());
-                }
-
-                return rezultatFormatie;
-            }
-        });
-
-        System.out.println("\n--- Lista studentilor sortati ---");
-        for (Student s : listaStudenti) {
+        System.out.println(" Lista studentilor actualizata cu note ");
+        for (Student s : mapStudenti.values()) {
             System.out.println(s);
         }
+        float notaM = gasesteNota("Bianca", "Popescu", mapStudenti);
+        float notaN = gasesteNota("loan", "Popa", mapStudenti);
 
-        try {
-            List<String> liniiDeScris = new ArrayList<>();
-            for (Student s : listaStudenti) {
-                liniiDeScris.add(s.toString());
-            }
+        System.out.println("Nota Bianca: " + notaM);
+        System.out.println("Nota loan: " + notaN);
+    }
 
-            Path pathIesire = Paths.get(fisierIesire);
-            Files.write(pathIesire, liniiDeScris);
+    public static float gasesteNota(String prenume, String nume, Map<String, Student> mapInitial) {
 
-            System.out.println("\nDatele au fost salvate in: " + fisierIesire);
+        Map<String, Student> mapDupaNume = new HashMap<>();
 
-        } catch (IOException e) {
-            System.out.println("Eroare la scrierea fisierului!");
-            e.printStackTrace();
+        for (Student s : mapInitial.values()) {
+            String cheieNoua = s.getPrenume() + "-" + s.getNume();
+            mapDupaNume.put(cheieNoua, s);
         }
+        String cheieCautata = prenume + "-" + nume;
+        Student studentCautat = mapDupaNume.get(cheieCautata);
+
+        if (studentCautat != null) {
+            return studentCautat.getNota();
+        }
+        return 0.0f;
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -5,34 +5,45 @@ public class Student {
     private String nume;
     private String prenume;
     private String formatieDeStudiu;
+    private float nota;
 
-    public Student (String nr_matricol,String nume,String prenume,String formatieDeStudiu){
-        this.nr_matricol=nr_matricol;
-        this.nume=nume;
-        this.prenume=prenume;
-        this.formatieDeStudiu=formatieDeStudiu;
+    public Student(String nr_matricol, String nume, String prenume, String formatieDeStudiu) {
+        this.nr_matricol = nr_matricol;
+        this.nume = nume;
+        this.prenume = prenume;
+        this.formatieDeStudiu = formatieDeStudiu;
+        this.nota = 0.0f;
+    }
+    public void setNota(float nota){
+        this.nota=nota;
+    }
+    public float getNota(){
+        return nota;
+    }
+    public String getNume(){
+        return nume;
+    }
+    public String getPrenume(){
+        return prenume;
+    }
+    public String getFormatieDeStudiu(){
+        return formatieDeStudiu;
     }
     @Override
     public String toString() {
-        return "Student { matricol='" + nr_matricol + "', nume='" + nume + "', prenume='" + prenume + "', grupa='" + formatieDeStudiu + "' }";
+        return "Student { matricol='" + nr_matricol + "', nume='" + nume + "', prenume='" + prenume + "', grupa='" + formatieDeStudiu + "', nota=" + nota + " }";
     }
     @Override
-    public boolean equals(Object s){
-        if(this == s) return true;
-        if(s == null || getClass()!=s.getClass()) return false;
-        Student student= (Student) s;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
         return Objects.equals(nume, student.nume) &&
                 Objects.equals(prenume, student.prenume) &&
                 Objects.equals(formatieDeStudiu, student.formatieDeStudiu);
     }
     @Override
-    public int hashCode(){
-        return Objects.hash(nume,prenume,formatieDeStudiu);
-    }
-    public String getNume() {
-        return nume;
-    }
-    public String getFormatieDeStudiu() {
-        return formatieDeStudiu;
+    public int hashCode() {
+        return Objects.hash(nume, prenume, formatieDeStudiu);
     }
 }
