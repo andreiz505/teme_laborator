a96209a86f3c9760901015a1e38885eed71bc931
Andrei Zamfirescu <andrzamfirescu20@gmail.com>
2026-05-12 17:13:11 +0300

    Tema casa laborator 9

--- src/Problema2.java
+++ src/Problema2.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 
 public class Problema2 {
     public static void main(String[] args) {
-        String text ;
+        String text = "Acesta este un program scris pentru expresii lambda";
         List<String> cuvinte = Arrays.asList(text.split(" "));
 
         List<String> filtrate = cuvinte.stream()
